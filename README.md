02ae7718eedd3c710fafd577cd2d1d78d782dc50
Cameron Bulock <cameron@bulock.com>
2013-02-13 15:18:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# LESS Theme Generator
+
+The general idea behind this is to create a directory that is laid out with files titled THEMENAME.less and then subdirectories named THEMENAME.
+
+This script will go through that directory and generate CSS using LESS for each of those THEMENAME.less files and place them in the subdirectories.
+
+This is helpful as those .less files can be specific to a theme, and then include other .less files that encompass the broder LESS that will be used for all themes.
---
+++ lesstheme.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+args = process.argv
+
+var theme_loc = '.';
+if (args.length > 2) {
+ theme_loc = theme_loc + '/' + args[2];
+}
+theme_loc = theme_loc + '/';
+
+var fs = require('fs');
+var sys = require('util');
+var exec = require('child_process').exec;
+
+var puts = function(err, stdout, stderr) {
+	if (stderr) {
+		throw stderr;
+	}
+	else {
+		sys.puts('CSS generated');
+	}
+}	
+
+dirlist = function(dir) {
+	var list = fs.readdirSync(dir);
+	var dirlist = [];
+	for(var i in list) {
+		if (!~list[i].indexOf(".")) dirlist.push(list[i])
+	}
+	return dirlist;
+}
+
+dl = dirlist(theme_loc)
+
+for(var i in dl) {
+	exec('lessc ' + theme_loc + dl[i] + '.less > ' + theme_loc + dl[i] + '/' + dl[i] + '.css', puts);
+}
