c6986757db761b331c3804edfb49c2abd806a4c7
Cameron Bulock <cameron@bulock.com>
2015-03-19 14:32:05 -0400

    complete refactor of how this works

--- lesstheme.js
+++ lesstheme.js
@@ -1,37 +1,53 @@
 #!/usr/bin/env node
 
+var less = require('less');
+var fs = require('fs');
+
 args = process.argv
 
-var theme_loc = '.';
+var less_dir = '.';
 if (args.length > 2) {
- theme_loc = theme_loc + '/' + args[2];
+ less_dir = less_dir + '/' + args[2];
 }
-theme_loc = theme_loc + '/';
+less_dir = less_dir + '/';
+var css_dir = less_dir + "../css/";
 
-var fs = require('fs');
-var sys = require('util');
-var exec = require('child_process').exec;
-
-var puts = function(err, stdout, stderr) {
-	if (stderr) {
-		throw stderr;
-	}
-	else {
-		sys.puts('CSS generated');
-	}
-}	
-
-dirlist = function(dir) {
-	var list = fs.readdirSync(dir);
-	var dirlist = [];
+var find_less_files = function( less_dir ) {
+	var list = fs.readdirSync( less_dir );
+	var lessfiles = [];
 	for(var i in list) {
-		if (!~list[i].indexOf(".")) dirlist.push(list[i])
+		var a = list[i].split('.');
+		var ext = a[a.length - 1];
+		var base = a[0];
+		if (ext == 'less') lessfiles.push(base);
 	}
-	return dirlist;
+	return lessfiles;
 }
 
-dl = dirlist(theme_loc)
+var themes = require( less_dir + 'themes.json' ).themes;
+var less_files = find_less_files( less_dir );
 
-for(var i in dl) {
-	exec('lessc --source-map ' + theme_loc + dl[i] + '.less ' + theme_loc + dl[i] + '/' + dl[i] + '.css', puts);
+for ( var t in themes ) {
+	var css_theme_dir = css_dir + themes[t] + '/';
+	if (!fs.existsSync(css_theme_dir)){
+		fs.mkdirSync(css_theme_dir);
+	}
+	for ( var f in less_files ) {
+		//read in each less file
+		var less_filename = less_dir + less_files[f] + ".less";
+		var css_filename = css_theme_dir + less_files[f] + ".css";
+		var sourcemap_filename = css_filename + ".map";
+		var data = fs.readFileSync( less_filename, "utf8");
+		//process the less
+		less.render( data, {
+			sourceMap: true,
+			compress: true,
+		}, function( err, output ) {
+			//write out the files
+			fs.writeFileSync( css_filename, output.css);
+			fs.writeFileSync( sourcemap_filename, output.map);
+		});
+	};
 }
+
+console.log('CSS Output complete');
