7d08d231b11c1cf07cbe4f2b9705404365b3a6fa
Cameron Bulock <cameron@bulock.com>
2015-03-20 14:17:03 -0400

    finally got everything working probably with the async file generation

--- lesstheme.js
+++ lesstheme.js
@@ -2,6 +2,7 @@
 
 var less = require('less');
 var fs = require('fs');
+var path = require('path');
 
 args = process.argv
 
@@ -9,8 +10,8 @@ var less_dir = process.cwd();
 if (args.length > 2) {
  less_dir = less_dir + '/' + args[2];
 }
-less_dir = less_dir + '/';
-var css_dir = less_dir + "../css/";
+less_dir = path.resolve(less_dir + '/');
+var css_dir = path.resolve(less_dir + "/../css/");
 
 var find_less_files = function( less_dir ) {
 	var list = fs.readdirSync( less_dir );
@@ -24,37 +25,39 @@ var find_less_files = function( less_dir ) {
 	return lessfiles;
 }
 
-var themes = require( less_dir + 'themes.json' ).themes;
+var themes = require( less_dir + '/themes.json' ).themes;
 var less_files = find_less_files( less_dir );
 
 for ( var t in themes ) {
-	var css_theme_dir = css_dir + themes[t] + '/';
+	var css_theme_dir = path.resolve(css_dir + "/" + themes[t]);
 	if (!fs.existsSync(css_theme_dir)){
 		fs.mkdirSync(css_theme_dir);
 	}
 	for ( var f in less_files ) {
 		//read in each less file
-		var less_filename = less_dir + less_files[f] + ".less";
-		var css_filename = css_theme_dir + less_files[f] + ".css";
+		var less_filename = less_dir + "/" + less_files[f] + ".less";
+		var css_filename = css_theme_dir + "/" + less_files[f] + ".css";
 		var sourcemap_filename = css_filename + ".map";
 		var data = fs.readFileSync( less_filename, "utf8");
-		//process the less
-		less.render( data, {
-			sourceMap: true,
-			compress: true,
-			globalVars: {
-				theme: themes[t]
-			},
-		}, function( err, output ) {
-			if (err) {
-				console.log( "ERR: " + err.message );
-			} else {
-				//write out the files
-				fs.writeFileSync( css_filename, output.css);
-				fs.writeFileSync( sourcemap_filename, output.map);
-			}
-		});
+    var render = function(data, css_filename, sourcemap_filename) {
+      //process the less
+      less.render( data, {
+        sourceMap: true,
+        compress: true,
+        globalVars: {
+          theme: themes[t]
+        },
+      }, function( err, output ) {
+        if (err) {
+          console.log( "ERR: " + err.message );
+        } else {
+          //write out the files
+          fs.writeFileSync( css_filename, output.css);
+          fs.writeFileSync( sourcemap_filename, output.map);
+          console.log( css_filename + " generated.");
+        }
+      });
+    };
+    render(data, css_filename, sourcemap_filename);
 	};
 }
-
-console.log('CSS Output complete');
